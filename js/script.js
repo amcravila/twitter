15f6b96b3b5a0cc6b700a1c008f1c46fb4186047
amcravila <amcravila@gmail.com>
2018-07-07 19:31:29 -0300

    VERSÃO 0.0.3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-var text = document.querySelector('#textTwitter');
+var text = document.getElementById('textTwitter');
+var btn = document.getElementById('tweetar');
+btn.addEventListener('click', onClickTweetar);
+text.addEventListener('keyup', disabledBtn);
 
 function onClickTweetar() {
   var textMsg = text.value;
@@ -6,41 +9,22 @@ function onClickTweetar() {
   event.preventDefault();
 }
 
-function limitTextarea(value) {
-    quant = 140;
-    total = value.length;
-    if(total <= quant) {
-        rest = quant - total;
-        document.getElementById('counter').innerHTML = rest;
-    } else {
-        document.getElementById('textTwitter').value = value.substr(0,quant);
-    }
+function disabledBtn() {
+  var quant = 140;
+  var textMsg = text.value;
+  var rest = quant - textMsg.length;
+  document.getElementById('counter').innerHTML = rest;
+  if (textMsg.length <= quant) {
+    btn.removeAttribute('disabled');
+  }
+  if (textMsg.length > quant) {
+    btn.setAttribute('disabled', 'true');
+    document.getElementById('counter').style.color = '#ff0000';
+  }
+  if (textMsg.length > 130 && textMsg.length <= 140) {
+    document.getElementById('counter').style.color = '#0000ff';
+  }
+  if (textMsg.length > 120 && textMsg.length <= 130) {
+    document.getElementById('counter').style.color = '#ff00ff';
+  }
 }
-
-// var tweet = document.getElementsByClassName("tweets")[0];
-// var myButton = document.getElementsByClassName("btn")[0];
-// var mensage = document.getElementById("mensages");
-// var paragraph = document.getElementById("paragraphs");
-//
-//
-// tweet.addEventListener("keyup", textEntered);
-//
-// function textEntered(/*event*/){
-//     myButton.setAttribute("class", "pressed");
-//     event.preventDefault();
-// }
-// console.log(myButton);
-//
-// // myButton.addEventListener("click", transformInTweet());
-//
-// myButton.addEventListener("click", event => {
-//     event.preventDefault();
-//    transformInTweet();
-// });
-//
-// function transformInTweet(){
-//     var div = document.createElement("div");
-//     div.className = "post";
-//     div.innerHTML = tweet.value;
-//     mensage.appendChild(div);
-// }
